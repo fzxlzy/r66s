d372bbed179f2ef3dad53743bd42d6a5f27f1c16
fzxlzy <38089446+fzxlzy@users.noreply.github.com>
2026-06-24 09:55:13 +0800

    Update environment.sh

--- scripts/environment.sh
+++ scripts/environment.sh
@@ -1,7 +1,8 @@
-sudo apt install -y ack antlr3 asciidoc autoconf automake autopoint binutils bison build-essential \
-bzip2 ccache cmake cpio curl device-tree-compiler fastjar flex gawk gettext gcc-multilib g++-multilib \
-git gperf haveged help2man intltool libc6-dev-i386 libelf-dev libfuse-dev libglib2.0-dev libgmp3-dev \
-libltdl-dev libmpc-dev libmpfr-dev libncurses5-dev libncursesw5-dev libpython3-dev libreadline-dev \
-libssl-dev libtool lrzsz mkisofs msmtp ninja-build p7zip p7zip-full patch pkgconf python3 \
-python3-pyelftools python3-setuptools qemu-utils rsync scons squashfs-tools subversion swig texinfo \
-uglifyjs upx-ucl unzip vim wget xmlto xxd zlib1g-dev
+sudo apt-get update -y
+sudo apt-get install -y \
+  build-essential clang flex bison g++ gawk gcc-multilib gettext git libncurses-dev \
+  libssl-dev python3-setuptools rsync swig unzip zlib1g-dev file wget curl \
+  ccache rsync subversion vim python3 python3-pyelftools \
+  libelf-dev autoconf automake libtool autopoint cmake texinfo cpio \
+  pkgconf device-tree-compiler mkisofs msmtp libglib2.0-dev xsltproc \
+  fastjar ninja-build uglifyjs qemu-utils upx
